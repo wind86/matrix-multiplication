ccc857ff25abfc70cd90359b19d73ccc2d524749
wind86 <wind86@meta.ua>
2017-10-05 12:13:49 +0300

    prevent converting double[][] to Double[][] in stream multiplier

--- src/main/java/com/matrix/multiplication/StreamMatrixMultiplier.java
+++ src/main/java/com/matrix/multiplication/StreamMatrixMultiplier.java
@@ -1,25 +1,23 @@
 package com.matrix.multiplication;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
-
 import com.matrix.Matrix;
 
 public class StreamMatrixMultiplier implements MatrixMultiplier {
 
 	@Override
 	public Matrix<? extends Number> multiply(Matrix<? extends Number> m1, Matrix<? extends Number> m2) {
-		Double[][] result = Arrays.stream(m1.getData()).parallel().map(rw ->
-			IntStream.range(0, m2.getColumnsSize())
-				.mapToDouble(j ->
-					IntStream.range(0, m2.getRowsSize())
-						.mapToDouble(k -> rw[k].doubleValue() * m2.getElement(k, j).doubleValue())
-						.sum()
-				)
-			.mapToObj(value -> new Double(value))
-			.toArray(Double[]::new)
-		).toArray(Double[][]::new);
+		Matrix<Double> matrix = new Matrix<>(Double.class, m1.getRowsSize(), m2.getColumnsSize());
 		
-		return new Matrix<Double>(Double.class, result);
+		IntStream.range(0, m1.getRowsSize()).forEach(i ->
+			IntStream.range(0, m2.getColumnsSize())
+				.forEach(j -> matrix.setElement(i, j, 
+						IntStream.range(0, m2.getRowsSize())
+							.mapToDouble(k -> m1.getElement(i, k).doubleValue() * m2.getElement(k, j).doubleValue())
+							.sum()
+						))
+		);
+		
+		return matrix;
 	}
 }
